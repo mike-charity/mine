3e47d87f0af3822d01412ccd25f93f12d30e8a6b
mike-charity <Mikecharity0@gmail.com>
2023-10-06 23:18:40 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <h1>Weather</h1>
       <Weather />
+      <footer>
+      This project was coded by{" "}
+      <a href=" mikecharity0@gmail.com" target="_blank">
+        mikecharity0@gmail.com
+      </a>{" "}
+      and is{" "}
+      <a href="https://github.com/mike-charity/mine" target="_blank">
+        open-sourced on Github
+      </a>
+    </footer>
+  
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import App from "./App";
 import Weather from "./Weather";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1> Weather </h1>
-    <Weather city="paris" />
-    <footer>
-      This project was coded by{" "}
-      <a href=" mikecharity0@gmail.com" target="_blank">
-        mikecharity0@gmail.com
-      </a>{" "}
-      and is{" "}
-      <a href="https://github.com/mike-charity/mine" target="_blank">
-        open-sourced on Github
-      </a>
-    </footer>
+    
+    <App />
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
